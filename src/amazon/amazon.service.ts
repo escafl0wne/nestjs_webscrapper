575abde38b672aae111d5a826e9323e5400122b2
escafl0wne <falladvance@outlook.com>
2023-10-11 17:16:11 +0500

    added new modules and finished scraping logic

--- src/amazon/amazon.service.ts
+++ src/amazon/amazon.service.ts
@@ -1,6 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import puppeteer, { Browser } from 'puppeteer-core';
+
 
 @Injectable()
 export class AmazonService {
-     async getProducts(product:string) {}
+    constructor(private readonly configService:ConfigService){
+
+    }
+     async getProducts(product:string) {
+        let browser:Browser
+       
+        try {
+            
+            browser = await puppeteer.connect({
+                browserWSEndpoint: this.configService.get<string>('SBR_WS_ENDPOINT'),
+    
+            }); 
+            console.log(browser)
+            const page = await browser.newPage();
+            page.setDefaultNavigationTimeout(2*60*1000)
+            await Promise.all([
+                page.waitForNavigation(),
+                page.goto('https://www.amazon.com')
+            ])
+            await page.type('#twotabsearchtextbox',product)
+            await Promise.all([
+                page.waitForNavigation(),
+                page.click('#nav-search-submit-button')
+            ])
+            return await page.$$eval(".s-search-results .s-card-container",(resultItmes)=>{
+                return resultItmes.map((resultItem)=>{ 
+                    const url = resultItem.querySelector("a").href
+                    const title = resultItem.querySelector("s-title-instructions-style span")?.textContent
+                    const price = resultItem.querySelector(".a-price a-offscreen")?.textContent
+                    return {
+                        url,title,price
+                    }
+                })
+            })
+        }catch(e){console.log(e)
+         throw new HttpException(e.message,500)   
+        }finally{
+            if(browser)  await browser.close();
+         
+        
+        }
+       
+    }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AmazonModule } from './amazon/amazon.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 
 @Module({
   imports: [
-    
+    ConfigModule.forRoot({isGlobal: true}),
     AmazonModule],
   controllers: [],
   providers: [],
